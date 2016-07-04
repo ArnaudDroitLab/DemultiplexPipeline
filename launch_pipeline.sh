066c945a2380cd55f546a95d572ccbd3d10ba740
Eric Fournier <Fournier.Eric.2@crchudequebec.ulaval.ca>
2016-07-04 14:18:04 -0400

    Clean-up

--- launch_pipeline.sh
+++ launch_pipeline.sh
@@ -3,11 +3,13 @@
 # A POSIX variable
 OPTIND=1         # Reset in case getopts has been used previously in the shell.
 
-# Initialize our own variables:
-output="output"
-samplesheet="SampleSheet.csv"
-base_path=""
-lane='.*'
+# Initialize variables with default values
+output="output"                 # Directory where output should be stored.
+samplesheet="SampleSheet.csv"   # Path to the samplesheet file.
+base_path=""                    # Base path for the sequencer produced files.
+lane='.*'                       # Regular expression for processing only a subset of the lanes or tiles.
+
+# Parse parameters from the command line.
 while getopts "i:o:s:l:" opt; do
     case "$opt" in
     i)  base_path=$OPTARG
@@ -22,25 +24,17 @@ done
 
 shift $((OPTIND-1))
 
-echo $base_path
-echo $samplesheet
-echo $output
-workdir=$output
-#exit # haha securité
+# STEP 1 DEMULTIPLEXING #######################################################
 
-# STEP 1 DEMULTIPLEXING
-
-#base_path=/u01/genomique/Instruments/Illumina_HiSeq_Heaven/Raw_data/160620_D00487S_0119_AC8KJMANXX
-#workdir=/home/mvallee/Prostate_weekend
-
-mkdir -p $workdir
+# Name and create demultiplexing input and output directories.
+mkdir -p $output
 current_date=$(date +%Y-%m-%d)
-#samplesheet=$workdir/SampleSheet.csv
 dir_input=$base_path/Data/Intensities/BaseCalls
-dir_output=$workdir/Demultiplexed_"${current_date}"
+dir_output=$output/Demultiplexed_"${current_date}"
 
 base_mask="Y*,I6n*,Y*"
 
+# Generate the make files which will do the demultiplexing.
 /home/nanuq-admin/nanuq-programs/software/bcl2fastq-1.8.4/bin/configureBclToFastq.pl \
 --input-dir ${dir_input} \
 --output-dir ${dir_output} \
@@ -50,117 +44,116 @@ base_mask="Y*,I6n*,Y*"
 --tiles 's_'$lane'_.*' \
 --fastq-cluster-count 0
 
+# Preform demultiplexing.
 pushd ${dir_output}
-
 make -j 8
-popd
+popd# STEP 2 TRIMMING #############################################################
 
+mkdir $output/trim
+FILE_ADAPTORS=adaptors.fa
 
-# STEP 2 TRIMMING
-
-mkdir $workdir/trim
-FILE_ADAPTORS=/home/mvallee/Prostate_weekend/adaptors.fa
-
-for file in $(ls $workdir/Demultiplexed_*/Project_*/*/*_R1_001.fastq.gz)
+# Trim all demultiplexed files in parrallel, then wait on the jobs to complete.
+for file in $(ls $output/Demultiplexed_*/Project_*/*/*_R1_001.fastq.gz)
 do
 	sampleID=$(basename $file _R1_001.fastq.gz)
 
 	java -XX:ParallelGCThreads=1 -jar /home/nanuq-admin/nanuq-programs/software/trimmomatic/Trimmomatic-0.32/trimmomatic-0.32.jar \
 	PE -phred33 -threads 8 \
-	$workdir/Demultiplexed_*/Project_*/*/${sampleID}_R1_001.fastq.gz \
-    $workdir/Demultiplexed_*/Project_*/*/${sampleID}_R2_001.fastq.gz \
-	$workdir/trim/${sampleID}_trim_R1.fastq.gz $workdir/trim/${sampleID}_trim_R1_unpaired.fastq.gz \
-	$workdir/trim/${sampleID}_trim_R2.fastq.gz $workdir/trim/${sampleID}_trim_R2_unpaired.fastq.gz \
+	$output/Demultiplexed_*/Project_*/*/${sampleID}_R1_001.fastq.gz \
+    $output/Demultiplexed_*/Project_*/*/${sampleID}_R2_001.fastq.gz \
+	$output/trim/${sampleID}_trim_R1.fastq.gz $output/trim/${sampleID}_trim_R1_unpaired.fastq.gz \
+	$output/trim/${sampleID}_trim_R2.fastq.gz $output/trim/${sampleID}_trim_R2_unpaired.fastq.gz \
 	ILLUMINACLIP:${FILE_ADAPTORS}:2:30:10 \
 	TRAILING:30 MINLEN:36 &
 done
 wait
 
+# STEP 3 ALIGNMENT ############################################################
 
-# SE TEST
-# for file in $(ls $workdir/Demultiplexed_*/Project_*/*/*_R1_001.fastq.gz)
-# do
-# 	sampleID=$(basename $file _R1_001.fastq.gz)
-# 
-# 	java -XX:ParallelGCThreads=1 -jar /home/nanuq-admin/nanuq-programs/software/trimmomatic/Trimmomatic-0.32/trimmomatic-0.32.jar \
-# 	SE -phred33 -threads 8 \
-# 	$workdir/Demultiplexed_*/Project_*/*/${sampleID}_R1_001.fastq.gz \
-# 	$workdir/trim/${sampleID}_trim_R1.fastq.gz \
-# 	ILLUMINACLIP:${FILE_ADAPTORS}:2:30:10 \
-# 	TRAILING:30 MINLEN:36 &
-# done
-# wait
+mkdir -p $output/STAR/Log
+mkdir -p $output/STAR/Within
+mkdir -p $output/STAR/Unsorted
 
-# STEP 3 ALIGNING
-
-mkdir $workdir/STAR
-#cd $workdir/STAR
-mkdir $workdir/STAR/Log
-mkdir $workdir/STAR/Within
-mkdir $workdir/STAR/Unsorted
-
-	# 1st pass
-
-for file in $(ls $workdir/trim/*_trim_R1.fastq.gz)
+# STAR 1st pass
+for file in $(ls $output/trim/*_trim_R1.fastq.gz)
 do
 	sampleID=$(basename $file _trim_R1.fastq.gz)
 
-	mkdir -p $workdir/STAR/alignment_1stPass/${sampleID}/
+	mkdir -p $output/STAR/alignment_1stPass/${sampleID}/
 
 	/software/STAR/STAR-2.5.2a/bin/Linux_x86_64/STAR --runMode alignReads \
 		--genomeDir /is1/commonDatasets/STAR/Homo_sapiens.GRCh38/genome/star_index/Ensembl77.sjdbOverhang99 \
 		--readFilesIn \
-			$workdir/trim/${sampleID}_trim_R1.fastq.gz \
-			$workdir/trim/${sampleID}_trim_R2.fastq.gz \
+			$output/trim/${sampleID}_trim_R1.fastq.gz \
+			$output/trim/${sampleID}_trim_R2.fastq.gz \
 		--runThreadN 32 \
 		--readFilesCommand zcat \
 		--outStd Log \
 		--outSAMunmapped Within \
 		--outSAMtype BAM Unsorted \
-		--outFileNamePrefix $workdir/STAR/alignment_1stPass/${sampleID}/ \
+		--outFileNamePrefix $output/STAR/alignment_1stPass/${sampleID}/ \
 		--outSAMattrRGline ID:"${sampleID}"     PL:"ILLUMINA"             SM:"${sampleID}"     CN:"CRCHUQ"  \
 		--limitGenomeGenerateRAM 70000000000 \
 		--limitIObufferSize 1000000000 
 done
 
-	# Create new splice junction database containing the splice junctions of all samples
-
-cat $workdir/STAR/alignment_1stPass/*/SJ.out.tab | \
-awk 'BEGIN {OFS="\t"; strChar[0]="."; strChar[1]="+"; strChar[2]="-"} {if($5>0){print $1,$2,$3,strChar[$4]}}' | sort -k1,1h -k2,2n > $workdir/STAR/alignment_1stPass/AllSamples.SJ.out.tab && \
-mkdir -p $workdir/STAR/reference.Merged && \
+# Create new splice junction database containing the splice junctions of all samples
+cat $output/STAR/alignment_1stPass/*/SJ.out.tab | \
+awk 'BEGIN {OFS="\t"; strChar[0]="."; strChar[1]="+"; strChar[2]="-"} {if($5>0){print $1,$2,$3,strChar[$4]}}' | sort -k1,1h -k2,2n > $output/STAR/alignment_1stPass/AllSamples.SJ.out.tab && \
+mkdir -p $output/STAR/reference.Merged && \
 /software/STAR/STAR-2.5.2a/bin/Linux_x86_64/STAR --runMode genomeGenerate \
-	--genomeDir $workdir/STAR/reference.Merged \
+	--genomeDir $output/STAR/reference.Merged \
 	--genomeFastaFiles /is1/commonDatasets/STAR/Homo_sapiens.GRCh38/genome/Homo_sapiens.GRCh38.fa \
 	--runThreadN 32 \
 	--limitGenomeGenerateRAM 50000000000 \
-	--sjdbFileChrStartEnd $workdir/STAR/alignment_1stPass/AllSamples.SJ.out.tab \
+	--sjdbFileChrStartEnd $output/STAR/alignment_1stPass/AllSamples.SJ.out.tab \
 	--limitIObufferSize 1000000000 \
 	--sjdbOverhang 99
 
 
-	# 2nd pass
-
-for file in $(ls $workdir/trim/*_trim_R1.fastq.gz)
+# STAR 2nd pass.
+for file in $(ls $output/trim/*_trim_R1.fastq.gz)
 do
     sampleID=$(basename $file _trim_R1.fastq.gz)
-	mkdir -p $workdir/alignment/${sampleID}
+	mkdir -p $output/alignment/${sampleID}
 
 	/software/STAR/STAR-2.5.2a/bin/Linux_x86_64/STAR --runMode alignReads \
-		--genomeDir $workdir/STAR/reference.Merged \
+		--genomeDir $output/STAR/reference.Merged \
 		--readFilesIn \
-			$workdir/trim/${sampleID}_trim_R1.fastq.gz \
-			$workdir/trim/${sampleID}_trim_R2.fastq.gz \
-		--runThreadN 16 \
+			$output/trim/${sampleID}_trim_R1.fastq.gz \
+			$output/trim/${sampleID}_trim_R2.fastq.gz \
+		--runThreadN 32 \
 		--readFilesCommand zcat \
 		--outStd Log \
 		--outSAMunmapped Within \
 		--outSAMtype BAM SortedByCoordinate \
-		--outFileNamePrefix $workdir/alignment/${sampleID}/ \
+		--outFileNamePrefix $output/alignment/${sampleID}/ \
 		--limitGenomeGenerateRAM 70000000000 \
 		--limitBAMsortRAM 70000000000 \
 		--limitIObufferSize 1000000000 \
 		--outWigType wiggle read1_5p --outWigStrand Stranded --outWigReferencesPrefix chr 
 done
 
-# Counting reads
+# Perform QC ##################################################################
+module add fastqc/0.11.2
+mkdir $output/fastqc
+for file in $(ls $output/trim/*_R?.fastq.gz)
+do   
+    sequenceFile=`basename $file .fastq.gz`
+    mkdir -p $output/fastqc/$sequenceFile
+    fastqc $file -o $output/fastqc/$sequenceFile &
+done
+wait
 
+# Count reads ##################################################################
+module add python/2.7.8
+mkdir $output/counts
+for bamFile in $output/alignment/*/Aligned.sortedByCoord.out.bam
+do
+    sampleDir=`dirname $bamFile`
+    sampleID=`basename sampleDir`
+
+    /home/foueri01/.local/bin/htseq-count -s reverse --order pos -f bam $bamFile /is1/commonDatasets/STAR/Homo_sapiens.GRCh38/annotations/Homo_sapiens.GRCh38.Ensembl77.gtf > $output/counts/$sampleID.counts &
+done
+
+Rscript summary_table.R $output
